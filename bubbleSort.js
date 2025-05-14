5ddae267dcdbd5b2472befdaa923e498c34974fb
BarkerBrian-FS <bmbarker@student.fullsail.edu>
2025-05-14 14:19:33 -0500

    changed bubble sort pseudocode

--- bubbleSort.js
+++ bubbleSort.js
@@ -8,7 +8,7 @@ const swap (arr, idx1, idx2) {
     [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
 }*/
 // Bubble Sort
-// start looping with a variable called i at the end of the array towards the beginning
+// start looping with a variable called i at the beginning of the array
 // start an inner loop with a variable called j from the beginning until i -1
 // if arr[j] is greater than arr[j+1], swap those two values
 // return the sorted array
