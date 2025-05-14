d4b986d1bdca4ccf94209a49c1ed8fd425047958
BarkerBrian-FS <bmbarker@student.fullsail.edu>
2025-05-14 13:26:19 -0500

    added linear search function and pseudocode

---
+++ linearSearch.js
@@ -0,0 +1,16 @@
+//write a function accepting array and value
+//loop through array 
+//if the value is found, return index
+//if the value is not found, return -1
+function linearSearch(arr, val){
+    for(let i=0; i<arr.length; i++){
+        if(arr[i] === val){
+            return i;
+        }
+        return -1;
+    }
+}
+
+linearSearch([1,2,3,4,5], 3) // 2
+// Time complexity: O(n)
+// Space complexity: O(1)
