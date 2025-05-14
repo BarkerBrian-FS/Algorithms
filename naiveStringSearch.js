2513640cdf709d11c37fe5c2721ef0be5e091a63
BarkerBrian-FS <bmbarker@student.fullsail.edu>
2025-05-14 13:13:31 -0500

    finished naive search

--- naiveStringSearch.js
+++ naiveStringSearch.js
@@ -4,3 +4,19 @@
 //if characters match keep going 
 //if we complete the inner loop and find a match, increment the count of matches
 //return count 
+function naiveStringSearch(long, short) {
+    let count = 0;
+    for (let i=0; i<long.length; i++){
+        for (let j=0; j<short.length; j++){
+            if(long[i+j] !== short[j]){
+                break;
+            }
+            if(j === short.length -1){
+                cont++;
+            }
+        }
+    }
+    return count;
+}
+
+naiveStringSearch("lorie loled", "lol") // 2
