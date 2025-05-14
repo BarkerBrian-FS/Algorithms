9d2bb2edeb780b4b5789a6b641ea2370a31edcb7
BarkerBrian-FS <bmbarker@student.fullsail.edu>
2025-05-14 12:24:48 -0500

    finished pseudocode

--- naiveStringSearch.js
+++ naiveStringSearch.js
@@ -1,3 +1,6 @@
 //Loop over longer string
 //Check if the substring matches
 //if characters dont match, break out inner loop
+//if characters match keep going 
+//if we complete the inner loop and find a match, increment the count of matches
+//return count 
