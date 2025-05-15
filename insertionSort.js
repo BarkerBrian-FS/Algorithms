382c3b07207e15ad834d99871474856344adea87
BarkerBrian-FS <bmbarker@student.fullsail.edu>
2025-05-15 13:39:48 -0500

    added psedocode to insertion sort

---
+++ insertionSort.js
@@ -0,0 +1,4 @@
+//Start by picking second element in array 
+//compare it to the one before it if smaller swap 
+//Continue to next element and if it in the incorrect order iterate through left side and put in correct place
+//Repeat until array is sorted 
--- selectionSort.js
+++ selectionSort.js
@@ -12,9 +12,11 @@ function selectionSort(arr){
                 lowest = j;
             }
         }
-        let temp = arr[i];
-        arr[i] = arr[lowest];
-        arr[lowest] = temp;
+        if(i !==lowest){
+            let temp = arr[i];
+            arr[i] = arr[lowest];
+            arr[lowest] = temp;
+        }
     }
     return arr;
 }
