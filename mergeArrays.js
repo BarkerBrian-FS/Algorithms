c9ac0c07e542aedbcc5b89c66d89cc6c599387ef
BarkerBrian-FS <bmbarker@student.fullsail.edu>
2025-05-16 12:14:23 -0500

    added new merge sort file

--- mergeArrays.js
+++ mergeArrays.js
@@ -7,7 +7,7 @@
   // If the value in the first array is larger than the value in the second push the value in the second
   // into our results and move on to the next value in second array
   // Once we exhaust one array push remaining values from the other array
-function mergSort(arr1, arr2){
+function merge(arr1, arr2){
     let results = [];
     let i = 0;
     let j = 0;
@@ -31,6 +31,6 @@ function mergSort(arr1, arr2){
         return results;
     }
     
-console.log(mergSort([1, 10, 50], [2, 14, 99, 100])) // [1, 2, 10, 14, 50, 99, 100]
+console.log(merge([1, 10, 50], [2, 14, 99, 100])) // [1, 2, 10, 14, 50, 99, 100]
 // Time complexity: O(n + m) where n and m are the lengths of the two arrays
 // Space complexity: O(n + m) where n and m are the lengths of the two arrays
