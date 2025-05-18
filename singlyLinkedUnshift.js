39bba56389cd73ce176e1a17bcf05cc42b8bbddf
BarkerBrian-FS <bmbarker@student.fullsail.edu>
2025-05-18 16:40:36 -0500

    added pseudocode for unshift

---
+++ singlyLinkedUnshift.js
@@ -0,0 +1,6 @@
+//function should accept a value 
+//create a new node using the value passed to the function 
+//if there is no head property on the list, set the head and tail to be the new node
+//otherwise set the head property to be the new node
+//increment the length by 1
+//return the list
