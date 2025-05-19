76af4bee2c5ecc9d9f8077ba11f8723c58ce551b
BarkerBrian-FS <bmbarker@student.fullsail.edu>
2025-05-19 13:13:21 -0500

    added pop function to singly linked list

--- singlyLinkedList.js
+++ singlyLinkedList.js
@@ -32,8 +32,39 @@ class SinglyLinkedList{
         this.length++
         return this;
     }
-}
+    //pop pseudo code
+    //If there are no nodes in list return undefined
+    //loop through the list until you reach the tail 
+    //set the next property of the second to last node to be null
+    //set the tail to be the second to last node 
+    //decrement the length by 1
+    //return the value of the node removed 
+    pop(){
+        if(!this.head) return undefined;
+        let current = this.head;
+        let newTail = current;
+        while(current.next){
+            newTail = current;
+            current = current.next;
+        }
+        this.tail = newTail;
+        this.tail.next = null;
+        this.length--;
+        if(this.length === 0){
+            this.head = null;
+            this.tail = null;
+        }
+        return current;
+        }
+    }
+    
 
 let list = new SinglyLinkedList();
 console.log(list.push(1));
 console.log(list.push(2));
+console.log(list.push(3));
+console.log(list.pop);
+
+
+
+
