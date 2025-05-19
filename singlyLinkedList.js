6bc83607fb7f896f3659794de87ca4b30dcc2748
BarkerBrian-FS <bmbarker@student.fullsail.edu>
2025-05-19 12:50:38 -0500

    added push to singly linked list

--- singlyLinkedList.js
+++ singlyLinkedList.js
@@ -1,6 +1,39 @@
-//function should accept a value 
-//create a new node using the value passed to the function 
-//if there is no head property on the list, set the head and tail to be the new node
-//otherwise set the head property to be the new node
-//increment the length by 1
-//return the list
+//
+
+class Node{
+    constructor(val){
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class SinglyLinkedList{
+    constructor(){
+        this.head = null;
+        this.tail= null;
+        this.length = 0;
+    }
+
+    //push pseudo code
+    //Function should accept a value
+    //Create new node with value passed to function 
+    //If there is no head property on the list, set the head and tail to the new value
+    //Otherwise set the next property on the tail to be the new node and set the tail property on the list to be the newly created node
+    //Increment the length by one
+    push(val){
+        let newNode = new Node(val);
+        if(!this.head){
+            this.head = newNode;
+            this.tail = this.head;
+        } else{
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.length++
+        return this;
+    }
+}
+
+let list = new SinglyLinkedList();
+console.log(list.push(1));
+console.log(list.push(2));
