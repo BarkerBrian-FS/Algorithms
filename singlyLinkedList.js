508cf31163d3ab66e5a1e1d8e3ac16fcb9e1719e
BarkerBrian-FS <bmbarker@student.fullsail.edu>
2025-05-20 13:07:11 -0500

    added get pseudo code and code to singly linked list

--- singlyLinkedList.js
+++ singlyLinkedList.js
@@ -92,7 +92,22 @@ class SinglyLinkedList{
         return this; 
         
     }
-}
+    //get pseudo code
+    //Function should accept an index
+    //If the index is less than zero or greater than or equal to the length of the list return null
+    //Loop through the list until you reach the index and return the node at that specific index
+    get(index){
+        if(index < 0 || index >= this.length) return null;
+        let count = 0;
+        let current = this.head;
+        while(count !== index){
+            current = current.next;
+            count++;
+        }
+        return current
+        };
+    }
+    //set pseudo code
 
   
 
